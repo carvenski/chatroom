a0b0b2b38c5404ebec22382ef7091568cc124acc
yxzoro <yangxing20107@gmail.com>
2018-03-20 18:07:28 +0800

    。

---
+++ server/HTTP_long_polling/chatroom_long_polling_server.py
@@ -0,0 +1,44 @@
+#encoding=utf8
+import web
+import time
+import gevent
+
+urls = (
+    '/get_messages', 'Hello1',
+    '/post_messages', 'Hello2'
+)
+
+app = web.application(urls, globals())
+messages = [] # [('hi', 121335), ('hello', 121334)]
+
+class Hello1:        
+    def GET(self):
+        REMOTE_ADDR = web.ctx.env['REMOTE_ADDR']
+        last_time = int(web.input().last_time)
+        global messages
+        web.header('Access-Control-Allow-Origin', '*')
+        t0 = time.time()
+        while True:
+            if not messages or int(messages[0][1]) <= last_time:
+                gevent.sleep(1)
+                if time.time() - t0 > 10:
+                    return ['****server no new message****']
+                continue
+            else:
+                i = 1
+                while i < (len(messages)-1) and int(messages[i][1]) > last_time:
+                    i += 1
+                return map(lambda x: x[0], filter(lambda y: y[2] != REMOTE_ADDR, messages[:i]))
+
+class Hello2:        
+    def GET(self):
+        REMOTE_ADDR = web.ctx.env['REMOTE_ADDR']
+        last_time = web.input().last_time; message = web.input().message
+        global messages
+        web.header('Access-Control-Allow-Origin', '*')
+        messages.append((message, last_time, REMOTE_ADDR))
+        messages.sort(key=lambda x: x[1], reverse=True)
+            
+
+WSGI_app = app.wsgifunc()
+
